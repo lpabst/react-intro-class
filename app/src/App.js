f0393ee2b83073f47d83a074f64e7868cc311227
loren pabst <loren pabst>
2017-06-12 11:07:24 -0600

    added code for calculator component

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator/Calculator.js';
 
 class App extends Component {
   render() {
     return (
-      
+      <Calculator />
     );
   }
 }
